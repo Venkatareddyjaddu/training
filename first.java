7c105b96889ffce6d17f38f7a79335561d068496
Venkata Reddy Jaddu <v.jaddu@zensar.com>
2022-04-22 13:42:13 +0530

    first

---
+++ first.java
@@ -0,0 +1,11 @@
+public class first{
+    public static void main(String[] args) {
+        double a = 50.9;
+        int b = -6;
+         
+         System.out.println("Sum =" + (a+b));
+         System.out.println("Subtraction =" + (a-b));
+         System.out.println("Division =" + (a/b));
+         System.out.println("Product =" + (a*b));
+    }
+}
