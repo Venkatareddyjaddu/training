c69dda4c74ba9b96126ce181561abfdd23310002
Venkata Reddy Jaddu <v.jaddu@zensar.com>
2022-06-14 15:31:14 +0530

    done

---
+++ EncapsulationDemo2.java
@@ -0,0 +1,7 @@
+public class EncapsulationDemo2 {
+    public static void main(String[] args) {
+        EncapsulationDemo ed = new EncapsulationDemo();
+        ed.setName("venkat");
+        System.out.println(ed.getName());
+    }
+}
